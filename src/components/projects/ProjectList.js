2937be9b70d5e84b69b0bf6131a4364093bb99d9
Eric Masi <eric@gitmasi.com>
2019-07-11 07:46:51 -0400

    add jsx

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const ProjectList = props => {
+  return (
+    <div className="project-list section">
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title Here</span>
+          <p>Posted By: Some Dude</p>
+          <p className="grey-text">Date Goes Here</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title Here</span>
+          <p>Posted By: Some Dude</p>
+          <p className="grey-text">Date Goes Here</p>
+        </div>
+      </div>
+      
+      <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title Here</span>
+          <p>Posted By: Some Dude</p>
+          <p className="grey-text">Date Goes Here</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Notifications;
